1137c5f065b68c27832d7c13f4d5285c600dd9f1
houdamzari <houda.mzari1999@gmail.com>
2023-04-15 00:59:06 +0000

    challenge solved

--- challenge.rb
+++ challenge.rb
@@ -1,4 +1,22 @@
 def simple_quicksort(array)
-  # write your code here
-  
+  return array if array.length <= 1
+
+  pivot = array[0]
+  left = []
+  right = []
+
+  array[1..-1].each do |num|
+    if num <= pivot
+      left << num
+    else
+      right << num
+    end
+  end
+
+  sorted_left = simple_quicksort(left)
+  sorted_right = simple_quicksort(right)
+
+  sorted = sorted_left + [pivot] + sorted_right
+  puts sorted.join(' ')
+  sorted
 end
